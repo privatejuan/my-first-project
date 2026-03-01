6985772cb97ff400a17ce6c1f620af078ea52692
Paul <privatepmj@proton.me>
2026-03-01 17:53:47 +1300

    Added food expense

--- lession1.py
+++ lession1.py
@@ -11,12 +11,24 @@ print("Name:", name)
 print("Age:", age)
 print("Fortnightly Income:", fortnightly_income)
 
-
 rent = 500
 remaining = fortnightly_income - rent
 
 print("Rent Costs:", rent)
 print("Remaining money:", remaining)
+
+food = 100
+spending = remaining - food 
+
+print(f"Food Budget: {food}")
+print(f"Remaining Money: {spending}")
+
+# Calcualte saving percentage
+
+savings = (spending / fortnightly_income) * 100
+
+print(f"Savings Percentage: {savings:.2f}%")
+
 
 
 
