c4fe023178e5f7436a2b0f27efa107ed6e65439f
Paul <privatepmj@proton.me>
2026-03-01 17:29:50 +1300

    Updated code

--- lession1.py
+++ lession1.py
@@ -3,32 +3,20 @@ print("I am learning Python!")
 
 name = "Paul"
 age = 27
-salary = 1600
+fortnightly_income= 1600
 
 # Variables: Data stored in memory.
 
 print("Name:", name)
 print("Age:", age)
-print("Salary:", salary)
+print("Fortnightly Income:", fortnightly_income)
 
 
 rent = 500
-remaining = salary - rent
+remaining = fortnightly_income - rent
 
 print("Rent Costs:", rent)
 print("Remaining money:", remaining)
-
-# Challenge: Calcualte fornightly income, rent and savings after rent.
-
-fornightly_income = salary / 2
-fornightly_rent = rent / 2
-fornightly_savings = fornightly_income - fornightly_rent
-rent_increase = rent + 50
-
-print("Fornightly Income:", fornightly_income)
-print("Fornightly Rent:", fornightly_rent)
-print("Fornightly Savings:", fornightly_savings)
-print("Rent after increase:", rent_increase)
 
 
 
